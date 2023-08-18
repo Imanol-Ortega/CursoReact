0d295eb00789389a307d987a48e265fc917a47aa
Imanol-Ortega <59693036+Imanol-Ortega@users.noreply.github.com>
2023-08-18 01:24:11 -0400

    se termino el twcard,ahora se inicio elTresEnRaya

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -1,15 +1,48 @@
 import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 import "./App.css"
+
+const users = [
+    {
+        UserName: 'imanolitoo',
+        Name: 'Imanol Ortega',
+        isFollowing: true
+
+    },
+    {
+        UserName: 'EddieOnLive',
+        Name: 'Eduardo Welchen',
+        isFollowing: true
+
+    },
+    {
+        UserName: 'alexnoaka',
+        Name: 'Alexander Rodas',
+        isFollowing: true
+
+    },
+]
+
 export function App(){
     return(
-        <section className="App">
-            <TwitterFollowCard userName={"imanolitoo"} isFollowing>
-                Imanol Ortega
-            </TwitterFollowCard>
-            <TwitterFollowCard userName={"EddieOnlive"} name={""} isFollowing={false}>
-                Eduardo Welchen
-            </TwitterFollowCard>
+        <div className="App">
+            {
+                users.map(({UserName,Name,isFollowing})=>{
+                    return (
+                        <TwitterFollowCard 
+
+                            key={UserName}
+                            userName={UserName}
+                            initialIsFollowing={isFollowing}
+
+                        >
+
+                            {Name}
+                            
+                        </TwitterFollowCard>
+                    )
+                })
+            }
             
-        </section>
+        </div>
     )
 }
